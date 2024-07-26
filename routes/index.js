47d29b1e84e040bf1afdcd9b5aaadbe78763cb10
Tiago Ferreira <ferreiratiago@outlook.com>
2024-07-26 16:37:11 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,18 @@ exports.index = function (req, res, next) {
 
 exports.loginHandler = function (req, res, next) {
   if (validator.isEmail(req.body.username)) {
-    User.find({ username: req.body.username, password: req.body.password, random: 3 }, function (err, users) {
+    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
+      if (users.length > 0) {
+        const redirectPage = req.body.redirectPage
+        const session = req.session
+        const username = req.body.username
+        return adminLoginSuccess(redirectPage, session, username, res)
+      } else {
+        return res.status(401).send()
+      }
+    });
+
+    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
       if (users.length > 0) {
         const redirectPage = req.body.redirectPage
         const session = req.session
